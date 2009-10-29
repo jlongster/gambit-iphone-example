54f427752406985167ae7b928023ca232a760201
James Long <james@coptix.com>
2009-10-29 12:19:28 -0400

    better image loading

--- lib/ffi/image.scm
+++ lib/ffi/image.scm
@@ -1,26 +1,35 @@
 
 (c-declare #<<end-c-code
 
-CGImageRef CGImageRef_loadpng(const char *filename) {
-    CFBundleRef mainBundle = CFBundleGetMainBundle();
-
-    CFStringRef name = CFStringCreateWithCString(NULL, filename, kCFStringEncodingUTF8);
-    CFURLRef url = CFBundleCopyResourceURL(mainBundle, name, CFSTR("png"), NULL);
-    CFRelease(name);
-
-    if(!url) {
-        return NULL;
-    }
-    
-    CGDataProviderRef provider = CGDataProviderCreateWithURL(url);
-    CFRelease(url);
-
-    CGImageRef image = CGImageCreateWithPNGDataProvider(provider, NULL, true,
-                                                        kCGRenderingIntentDefault);
-    CGDataProviderRelease(provider);
-    
-    return image;
+CGImageRef CGImageRef_load(const char *filename) {
+    NSString *path = [NSString stringWithFormat:@"%@/%s",
+                               [[NSBundle mainBundle] resourcePath],
+                               filename];
+    UIImage *img = [UIImage imageWithContentsOfFile:path];
+    if(img) return [img CGImage];
+    return NULL;
 }
+
+// CGImageRef CGImageRef_loadpng(const char *filename) {
+//     CFBundleRef mainBundle = CFBundleGetMainBundle();
+
+//     CFStringRef name = CFStringCreateWithCString(NULL, filename, kCFStringEncodingUTF8);
+//     CFURLRef url = CFBundleCopyResourceURL(mainBundle, name, CFSTR("pnx"), NULL);
+//     CFRelease(name);
+
+//     if(!url) {
+//         return NULL;
+//     }
+        
+//     CGDataProviderRef provider = CGDataProviderCreateWithURL(url);
+//     CFRelease(url);
+
+//     CGImageRef image = CGImageCreateWithPNGDataProvider(provider, NULL, true,
+//                                                         kCGRenderingIntentDefault);
+//     CGDataProviderRelease(provider);
+
+//     return image;
+// }
 
 unsigned char* CGImageRef_data(CGImageRef image) {
     NSInteger width = CGImageGetWidth(image);
@@ -38,7 +47,12 @@ unsigned char* CGImageRef_data(CGImageRef image) {
     CGContextDrawImage(context,
                        CGRectMake(0.0, 0.0, (float)width, (float)height),
                        image);
-	CGContextRelease(context);
+    CGContextRelease(context);
+
+    NSLog(@"[%d %d %d %d]", data[0], data[1], data[2], data[3]);
+    NSLog(@"[%d %d %d %d]", data[4], data[5], data[6], data[7]);
+    NSLog(@"[%d %d %d %d]", data[8], data[9], data[10], data[11]);
+    NSLog(@"[%d %d %d %d]", data[12], data[13], data[14], data[15]);
 
     return data;
 }
@@ -52,8 +66,11 @@ end-c-code
 (define CGImageRef-height
   (c-lambda (CGImageRef) int "___result = CGImageGetHeight(___arg1);"))
 
-(define CGImageRef-loadpng
-  (c-lambda (char-string) CGImageRef "CGImageRef_loadpng"))
+(define CGImageRef-load
+  (c-lambda (char-string) CGImageRef "CGImageRef_load"))
+
+;; (define CGImageRef-loadpng
+;;   (c-lambda (char-string) CGImageRef "CGImageRef_loadpng"))
 
 (define CGImageRef-data
   (c-lambda (CGImageRef) unsigned-int8-array "CGImageRef_data"))
